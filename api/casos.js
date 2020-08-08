27d6aec714e9a49b8d5fc4108ef4a3972d3a0ddb
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-08-08 20:22:42 -0300

    finish project

--- api/casos.js
+++ api/casos.js
@@ -7,19 +7,17 @@ router.get('/test', (req, res, next) => {
     res.json({status: 'ok'})
 })
 
-router.post('/:state/:dateStart/:dataEnd', async (req, res, next) => {
-    const { state, dateStart, dataEnd } = req.params
+router.post('/:state/:dateStart/:dateEnd', async (req, res, next) => {
+    const { state, dateStart, dateEnd } = req.params
     const cidadesArr = []
 
     const initialDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dateStart}&format=json&state=${state.toUpperCase()}`)
-    const finalDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dataEnd}&format=json&state=${state.toUpperCase()}`)
+    const finalDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dateEnd}&format=json&state=${state.toUpperCase()}`)
     
     const initialD = initialDate.data.results
     const finalD = finalDate.data.results
-    // console.log(finalD)
 
     for (let key in initialD) {
-        // console.log(finalD[key].city)
         cidadesArr.push({
             cidade: initialD[key].city,
             totalHabitantes: initialD[key].estimated_population_2019,
@@ -31,7 +29,17 @@ router.post('/:state/:dateStart/:dataEnd', async (req, res, next) => {
     const sorted = [...cidadesArr].sort((a, b) => b.porcentagem - a.porcentagem)
     const cidadesFiltradas = sorted.slice(0, 10)
 
-    res.status(200).json({ resultado: cidadesFiltradas })
+    res
+    .status(200)
+    .set('MeuNome', 'Camila')
+    .json({ 
+        resultado: cidadesFiltradas.map((resultado, index) => {
+            return {
+                id: index,
+                nomeCidade: resultado.cidade,
+                percentualDeCasos: resultado.porcentagem
+            }
+    }) })
 })
 
 module.exports = router
