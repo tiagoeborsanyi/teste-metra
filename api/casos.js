afafe5a8e9efa4100f1faff517e836fac8f4d2d1
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-08-08 19:36:01 -0300

    função de filtrar dados feita

--- api/casos.js
+++ api/casos.js
@@ -1,21 +1,37 @@
 const express = require('express')
-const request = require('request')
 const axios = require('axios')
 
 const router = express.Router()
 
-router.get('/test', async (req, res, next) => {
+router.get('/test', (req, res, next) => {
     res.json({status: 'ok'})
 })
 
 router.post('/:state/:dateStart/:dataEnd', async (req, res, next) => {
-    const { state, dateStart, dateEnd } = req.params
-    
+    const { state, dateStart, dataEnd } = req.params
+    const cidadesArr = []
 
     const initialDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dateStart}&format=json&state=${state}`)
-    const finalDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dateEnd}&format=json&state=${state}`)
-    console.log(initialDate.data.results)
-    res.status(200).json(initialDate.data.results)
+    const finalDate = await axios.get(`https://brasil.io/api/dataset/covid19/caso/data/?date=${dataEnd}&format=json&state=${state}`)
+    
+    const initialD = initialDate.data.results
+    const finalD = finalDate.data.results
+    // console.log(finalD)
+
+    for (let key in initialD) {
+        // console.log(finalD[key].city)
+        cidadesArr.push({
+            cidade: initialD[key].city,
+            totalHabitantes: initialD[key].estimated_population_2019,
+            casosInicial: initialD[key].confirmed,
+            casosFinal: finalD[key].confirmed,
+            porcentagem: (finalD[key].confirmed - initialD[key].confirmed) / initialD[key].estimated_population_2019 * 100
+        })
+    }
+    const sorted = [...cidadesArr].sort((a, b) => b.porcentagem - a.porcentagem)
+    const cidadesFiltradas = sorted.slice(0, 10)
+
+    res.status(200).json({ resultado: cidadesFiltradas })
 })
 
 module.exports = router
